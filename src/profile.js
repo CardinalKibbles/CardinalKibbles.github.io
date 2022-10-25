2b396319293af1fee4b4ff09a6284953770703e0
Tyler Giles <tylerjaygiles@gmail.com>
2022-10-25 01:53:03 -0700

    Minimum viable product
    UI and backend functioning

---
+++ src/profile.js
@@ -0,0 +1,179 @@
+// Change website animations
+const animation = {
+  // make it false to switch off fade-up animation
+  animate: true,
+  // animation playing duration
+  duration: 750,
+  // if true, animation plays only once when element comes on screen
+  once: false,
+};
+// Change your display name on tha landing display
+const header = {
+  name: "Tyler Giles",
+};
+const background = {
+  // Options: Snow or Particle
+  type: "Snow",
+};
+// Write a para about yourself here
+// To update your image, go to './styles/images.css'
+const section2title = "About Me";
+const about = {
+  paragraph:
+    "Web Developer who loves to create and weave connections. Graduated from PDX Code Guild Python Bootcamp, and continues to learn and expose be exposed to new concepts. Coming up on one year of developing and creating using JS and Python frameworks. Excited  and motivated engineer with ready to work on interesting projects!",
+};
+// Edit your skill and the percentage you know about it
+// To Add a skill, copy any one below and paste it after the last comma
+const skillsBar = [
+  {
+    name: "HTML5",
+    // To add a custom svg instead of font-awesome icons, add svg path below otherwise just comment it out
+    svg: "M0 32l34.9 395.8L191.5 480l157.6-52.2L384 32H0zm308.2 127.9H124.4l4.1 49.4h175.6l-13.6 148.4-97.9 27v.3h-1.1l-98.7-27.3-6-75.8h47.7L138 320l53.5 14.5 53.7-14.5 6-62.2H84.3L71.5 112.2h241.1l-4.4 47.7z",
+    faClass: "fab fa-html5",
+  },
+  {
+    name: "CSS3",
+    // svg: "",
+    faClass: "fab fa-css3",
+  },
+  {
+    name: "Javascript",
+    // svg: "",
+    faClass: "fab fa-js",
+  },
+  {
+    name: "Node",
+    // svg: "",
+    faClass: "fab fa-node",
+  },
+  {
+    name: "Python",
+    // svg: "",
+    faClass: "fab fa-python",
+  },
+  {
+    name: "Database",
+    // svg: "",
+    faClass: "fas fa-database",
+  },
+  {
+    name: "Bootstrap",
+    // svg: "M333.5,201.4c0-22.1-15.6-34.3-43-34.3h-50.4v71.2h42.5C315.4,238.2,333.5,225,333.5,201.4z M517,188.6 c-9.5-30.9-10.9-68.8-9.8-98.1c1.1-30.5-22.7-58.5-54.7-58.5H123.7c-32.1,0-55.8,28.1-54.7,58.5c1,29.3-0.3,67.2-9.8,98.1 c-9.6,31-25.7,50.6-52.2,53.1v28.5c26.4,2.5,42.6,22.1,52.2,53.1c9.5,30.9,10.9,68.8,9.8,98.1c-1.1,30.5,22.7,58.5,54.7,58.5h328.7 c32.1,0,55.8-28.1,54.7-58.5c-1-29.3,0.3-67.2,9.8-98.1c9.6-31,25.7-50.6,52.1-53.1v-28.5C542.7,239.2,526.5,219.6,517,188.6z M300.2,375.1h-97.9V136.8h97.4c43.3,0,71.7,23.4,71.7,59.4c0,25.3-19.1,47.9-43.5,51.8v1.3c33.2,3.6,55.5,26.6,55.5,58.3 C383.4,349.7,352.1,375.1,300.2,375.1z M290.2,266.4h-50.1v78.4h52.3c34.2,0,52.3-13.7,52.3-39.5 C344.7,279.6,326.1,266.4,290.2,266.4z",
+    faClass: "fab fa-bootstrap",
+  },
+  {
+    name: "Git",
+    // svg: "M100.59 334.24c48.57 3.31 58.95 2.11 58.95 11.94 0 20-65.55 20.06-65.55 1.52.01-5.09 3.29-9.4 6.6-13.46zm27.95-116.64c-32.29 0-33.75 44.47-.75 44.47 32.51 0 31.71-44.47.75-44.47zM448 80v352a48 48 0 0 1-48 48H48a48 48 0 0 1-48-48V80a48 48 0 0 1 48-48h352a48 48 0 0 1 48 48zm-227 69.31c0 14.49 8.38 22.88 22.86 22.88 14.74 0 23.13-8.39 23.13-22.88S258.62 127 243.88 127c-14.48 0-22.88 7.84-22.88 22.31zM199.18 195h-49.55c-25-6.55-81.56-4.85-81.56 46.75 0 18.8 9.4 32 21.85 38.11C74.23 294.23 66.8 301 66.8 310.6c0 6.87 2.79 13.22 11.18 16.76-8.9 8.4-14 14.48-14 25.92C64 373.35 81.53 385 127.52 385c44.22 0 69.87-16.51 69.87-45.73 0-36.67-28.23-35.32-94.77-39.38l8.38-13.43c17 4.74 74.19 6.23 74.19-42.43 0-11.69-4.83-19.82-9.4-25.67l23.38-1.78zm84.34 109.84l-13-1.78c-3.82-.51-4.07-1-4.07-5.09V192.52h-52.6l-2.79 20.57c15.75 5.55 17 4.86 17 10.17V298c0 5.62-.31 4.58-17 6.87v20.06h72.42zM384 315l-6.87-22.37c-40.93 15.37-37.85-12.41-37.85-16.73v-60.72h37.85v-25.41h-35.82c-2.87 0-2 2.52-2-38.63h-24.18c-2.79 27.7-11.68 38.88-34 41.42v22.62c20.47 0 19.82-.85 19.82 2.54v66.57c0 28.72 11.43 40.91 41.67 40.91 14.45 0 30.45-4.83 41.38-10.2z",
+    faClass: "fab fa-git",
+  },
+  {
+    name: "React",
+    // svg:"",
+    faClass: "fab fa-react"
+  },
+];
+// Edit your projects, its name, your skills used to make it, and the url.
+// You can omit freely anything if you dont have it
+// To Add a Project, copy any one below and paste it after the last comma and increment the id's project number
+const section3Title = "Past Projects";
+const projects = [
+  {
+    // Add image in './styles/images.css' in #project1
+    id: "project1",
+    name: "Meteorite Tracker",
+    skills: ["HTML, CSS, JS, Django, REST"],
+    url: "https://github.com/cardinalkibbles/Metorite-Tracker",
+  },
+  // {
+  //   // Add image in './styles/images.css' in #project2
+  //   id: "project2",
+  //   name: "Project 2",
+  //   skills: ["HTML, CSS, JS"],
+  //   url: "https://github.com/kaustubhai",
+  // },
+  // {
+  //   // Add image in './styles/images.css' in #project3
+  //   id: "project3",
+  //   name: "Project 3",
+  //   skills: ["HTML, CSS, JS"],
+  //   url: "https://github.com/kaustubhai",
+  // },
+  // {
+  //   // Add image in './styles/images.css' in #project4
+  //   id: "project4",
+  //   name: "Project 4",
+  //   skills: ["HTML, CSS, JS"],
+  //   url: "https://github.com/kaustubhai",
+  // },
+  // {
+  //   // Add image in './styles/images.css' in #project5
+  //   id: "project5",
+  //   name: "Project 5",
+  //   skills: ["HTML, CSS, JS"],
+  //   url: "https://github.com/kaustubhai",
+  // },
+  // {
+  //   // Add image in './styles/images.css' in #project6
+  //   id: "project6",
+  //   name: "Project 6",
+  //   skills: ["HTML, CSS, JS"],
+  //   url: "https://github.com/kaustubhai",
+  // },
+];
+// Edit your Miscellaneous Activities, its name and the url.
+// You can omit freely anything if you dont have it
+// To Add a Activity, copy any one below and paste it after the last comma and increment the id's Miscellaneous number
+const section4Title = "Development Education";
+const miscellaneous = [
+  {
+    // Add image in './styles/images.css' in #misc1
+    id: "misc1",
+    name: "PDX Code Guild",
+    url: "https://drive.google.com/file/d/1TAZEf0yB9GMK19qRjTMPKSKrifDP6hAZ/view?usp=sharing",
+  },
+  {
+    // Add image in './styles/images.css' in #misc2
+    id: "misc2",
+    name: "Free Code Camp",
+    url: "https://www.freecodecamp.org/TylerGiles",
+  },
+  // {
+  //   // Add image in './styles/images.css' in #misc3
+  //   id: "misc3",
+  //   name: "Miscellaneous 3",
+  //   url: "https://github.com/kaustubhai",
+  // },
+];
+// Contact form text, and Formspree link(to send a submit contact through their API as in contact.js)
+// To get your own jotform link, go to https://formspree.io/
+// If you hacve the link already, paste it in the contactUrl below
+const section5Title = "Get in Touch";
+const contact = {
+  pitch:
+    "If you would like to get in touch for collaborations or just to talk, I'd love to hear from you. Please fill out the form and I will respond as soon as possible. Feel free to download my resume below as a souvenir.",
+  copyright: "Tyler Giles",
+  contactUrl: "https://formspree.io/f/xbjbzjdn",
+};
+// Paste your respective social media links. You can omit any if you dont have it
+// Upload your resume in your drive, get the shaareable link and paste it in the resume section
+const social = {
+  github: "https://github.com/cardinalkibbles",
+  linkedin: "https://www.linkedin.com/in/tyler-giles-06778a13b/",
+  resume: "https://drive.google.com/file/d/1BSn_Z_9asTQNNDp9rcaFFLxPJYto8lnh/view?usp=sharing",
+};
+// Dont change anything here
+export {
+  animation,
+  header,
+  background,
+  about,
+  skillsBar,
+  projects,
+  miscellaneous,
+  contact,
+  social,
+  section2title,
+  section3Title,
+  section4Title,
+  section5Title,
+};
