cdaf705a60947a4cca875d4fcd153490674a9fe8
Tyler Giles <tylerjaygiles@gmail.com>
2022-10-26 09:19:13 -0700

    Updates

---
+++ precache-manifest.3d38bba0f3d59adfe4d07afa5f2d9475.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cb8d6719acc99c838de2",
+    "url": "/static/css/main.21a3379a.chunk.css"
+  },
+  {
+    "revision": "cb8d6719acc99c838de2",
+    "url": "/static/js/main.cb8d6719.chunk.js"
+  },
+  {
+    "revision": "2e9fea84eb9a2e27243a",
+    "url": "/static/css/1.3b2b192b.chunk.css"
+  },
+  {
+    "revision": "2e9fea84eb9a2e27243a",
+    "url": "/static/js/1.2e9fea84.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f70d015ccb3b244bf31c1a9ccbd6abc3",
+    "url": "/index.html"
+  }
+];
